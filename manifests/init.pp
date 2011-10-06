25db89483dab9d9777df0656c6418d987fa6f3c5
Ben Kero <ben.kero@gmail.com>
2011-10-06 16:54:57 +0000

    add class dependencies

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,17 @@
-class gitolite {
+class gitolite ($root="/var/lib/gitolite",
+                $user="gitolite,
+                $group="gitolite",
+                $sshkey = "ssh-rsa something user@host",
+    ) {
+
     include gitolite::packages
     include gitolite::config
     include gitolite::repos
+
+    class {
+        'gitolite::packages':
+            before => Class['gitolite::config'];
+        'gitolite::config':
+            before => Class['gitolite::repos'];
+    }
 }
