2b82f16910dba372316ab34c6d152fdad55062eb
Ben Kero <ben.kero@gmail.com>
2011-10-06 18:40:48 +0000

    add deps to file pathes

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,34 +1,42 @@
 class gitolite::config {
+    File {
+        owner => "$gitolite::user",
+        group => "$gitolite::group",
+        mode => 0600,
+    }
+
+    File['.gitolite'] -> File['gitolite-confdir' -> File['gitolite.conf']
+
     file {
+        ".gitolite":
+            ensure => directory,
+            path => "$gitolite::root/.gitolite";
+
+        "gitolite-confdir":
+            ensure => directory,
+            path => "$gitolite::root/.gitolite/conf",
+
         "gitolite.conf":
             ensure => present,
             path => "$gitolite::root/.gitolite/conf/gitolite.conf",
-            owner => "$gitolite::user",
-            group => "$gitolite::group",
-            mode => 0600,
+            require => File["gitolite-confdir"]
             content => template("gitolite/gitolite.conf.erb");
 
         "gitolite.rc":
             ensure => present,
             path => "$gitolite::root/.gitolite.rc",
-            owner => "$gitolite::user",
-            group => "$gitolite::group",
             mode => 0644,
             content => template("gitolite/gitolite.rc.erb");
 
         "ldap-group-query.sh":
             ensure => present,
             path => "/usr/local/bin/ldap-group-query.sh",
-            owner => "gitolite",
-            group => "gitolite",
             mode => 0700,
             source => "puppet:///modules/gitolite/ldap-group-query.sh";
 
         "gitweb.conf":
             ensure => present,
             path => "/etc/gitweb.conf",
-            owner => "root",
-            group => "root",
             mode => 0644,
             source => "puppet:///modules/gitolite/gitweb.conf",
 
