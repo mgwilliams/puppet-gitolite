cdfe442d97773124c2913b1a0f4e59716040f619
Ben Kero <ben.kero@gmail.com>
2011-10-07 10:53:53 +0000

    refactor class a bit, add gitolite-admin repo checkout

--- manifests/init-gitolite.pp
+++ manifests/init-gitolite.pp
@@ -1,19 +1,24 @@
 class gitolite::init-gitolite {
-    file { "masterkey.pub":
-        path => "$gitolite::root/masterkey.pub",
-        ensure => present,
-        owner => "$gitolite::user",
-        group => "$gitolite::group",
-        mode => 0600,
-        content => "$gitolite::sshkey";
-    }
 
-    exec { "creates-gitolite":
-        cwd => "$gitolite::root",
-        command => "/usr/bin/gl-setup -q $gitolite::root/masterkey.pub",
-        creates => "$gitolite::root/repositories",
-        user => "$gitolite::user",
-        environment => "HOME=$gitolite::root",
-        require => File["masterkey.pub"],
+    exec {
+        "create-keypair":
+            cwd => "$gitolite::root",
+            command "/usr/bin/ssh-keygen -q -t rsa -N '' -C 'Gitolite master key' -f $gitolite::root/.ssh/id_rsa",
+            creates => "$gitolite::root/.ssh/id_rsa",
+            user => "$gitolite::user",
+            environment => "HOME=$gitolite::root";
+
+        "creates-gitolite":
+            cwd => "$gitolite::root",
+            command => "/usr/bin/gl-setup -q $gitolite::root/.ssh/id_rsa.pub",
+            creates => "$gitolite::root/repositories",
+            user => "$gitolite::user",
+            environment => "HOME=$gitolite::root";
+        "clone-repository":
+            cwd => "$gitolite::root",
+            command => "/usr/bin/git clone $gitolite::root/repositories/gitolite-admin.git",
+            user => "$gitolite::user",
+            environment => "HOME=$gitolite::root";
     }   
+
 }
